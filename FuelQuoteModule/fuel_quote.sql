7a806ff03490a802035e4f96ab9b60850818cc98
theloudmute <42556429+theloudmute@users.noreply.github.com>
2021-03-31 10:50:23 -0500

    Update fuel_quote.sql

--- FuelQuoteModule/fuel_quote.sql
+++ FuelQuoteModule/fuel_quote.sql
@@ -1,39 +1,36 @@
 CREATE DATABASE IF NOT EXISTS fuel_company;
 use fuel_company;
 
-CREATE TABLE IF NOT EXISTS customers (
-	username varchar(50) not null primary key,
-    pwd varchar(50) not null,
-    lastname varchar(50) not null,
-    firstname varchar(50) not null,
-    address varchar(100) not null,
-    city varchar(50) not null,
-    state char(2) not null,
-    zip_code varchar(10)
+CREATE TABLE IF NOT EXISTS user_credentials (
+	id varchar(50) not null primary key;
+	password blob not null;
+);
+	
+CREATE TABLE IF NOT EXISTS client_information (
+	full_name varchar(50) not null,
+    	address1 varchar(100) not null,
+	address2 varchar(100),
+    	city varchar(100) not null,
+    	state char(2) not null,
+    	zip_code varchar(10) not null
 );
 
-insert into customers(username, pwd, lastname, firstname, address, city, state) 
-values('theloudmute', 'liltresky', 'Walters', 'LeDontre', '20906 Neelie Ct', 'Houston', 'TX');
-
-CREATE TABLE IF NOT EXISTS fuel_quote_history (
-	username varchar(50),
-	gallons int,
-    price decimal(4,2),
-    delivery_date date
-);
+insert into client_information(full_name, address1, city, state, zipcode) 
+values('LeDontre Walters', '20906 Neelie Ct', 'Houston', 'TX', '77778');
 
 DROP TABLE IF EXISTS fuel_quote;
 CREATE TABLE IF NOT EXISTS fuel_quote (
+	full_name varchar(50) not null,
 	gallons int not null,
-    delivery_date date,
-    address varchar(50) not null,
-    price decimal(4,2) not null,
-    cost numeric not null
+    	delivery_date date,
+    	address1 varchar(100) not null,
+    	price decimal(4,2) not null,
+    	cost numeric not null
 );
 
 CREATE TABLE IF NOT EXISTS price (
 	out_of_state_price decimal(4,2) not null,
-    in_state_price decimal(4,2) not null
+    	in_state_price decimal(4,2) not null
     );
 insert into price(out_of_state_price, in_state_price) values(2.49, 2.65);
     
